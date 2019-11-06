115b52ab7e979c17c52b4b26579243613f59a290
Mykola Demianchuk <nickdemianchuk@gmail.com>
2019-11-06 11:14:56 -0600

    refactor: properly name functions

--- median.py
+++ median.py
@@ -6,12 +6,12 @@ def contains_single_element(arr):
     return len(arr) == 1
 
 
-def is_even(arr):
+def is_even_length(arr):
     return len(arr) % 2 == 0
 
 
-def is_odd(arr):
-    return not is_even(arr)
+def is_odd_length(arr):
+    return not is_even_length(arr)
 
 
 def get_average(num1, num2):
@@ -20,14 +20,14 @@ def get_average(num1, num2):
 
 def get_median_index(arr):
     median_index = int(len(arr) / 2) - 1
-    if is_odd(arr):
+    if is_odd_length(arr):
         median_index = median_index + 1
     return median_index
 
 
 def get_median(arr):
     median_index = get_median_index(arr)
-    if is_even(arr):
+    if is_even_length(arr):
         median = get_average(arr[median_index], arr[median_index + 1])
     else:
         median = arr[median_index]
@@ -46,9 +46,9 @@ def get_median_single_element_array(arr, single_element_arr):
     arr_median_index = get_median_index(arr)
     single_element_arr_median_index = get_median_index(single_element_arr)
 
-    if is_odd(arr):
+    if is_odd_length(arr):
         median = arr[arr_median_index]
-        # since the merged array will be even, we need to find find two middle numbers
+        # since the merged array will be even length, we need to find find two middle numbers
         if arr[arr_median_index] < single_element_arr[single_element_arr_median_index]:
             next_to_median = min(arr[arr_median_index + 1], single_element_arr[single_element_arr_median_index])
         else:
@@ -86,10 +86,10 @@ def find_median(arr1, arr2):
     arr1_left_last_index = get_median_index(arr1)
     arr2_left_last_index = get_median_index(arr2)
 
-    # if both are odd it means that their left halfs would be 1 element bigger than the right
+    # if both are odd length it means that their left halfs would be 1 element bigger than the right
     # so if the first left half is bigger by one element
     # then we want to make sure the second one's right half is one element smaller than the left  half
-    if is_odd(arr1) and is_odd(arr2):
+    if is_odd_length(arr1) and is_odd_length(arr2):
         arr2_left_last_index = arr2_left_last_index - 1
 
     while True:
@@ -99,7 +99,7 @@ def find_median(arr1, arr2):
         if arr1[arr1_left_last_index] <= arr2[arr2_right_first_index] \
                 and arr2[arr2_left_last_index] <= arr1[arr1_right_first_index]:
             median = max(arr1[arr1_left_last_index], arr2[arr2_left_last_index])
-            # when the merged array is even return the average of two numbers in the middle
+            # when the merged array is even length, return the average of two numbers in the middle
             if (len(arr1) + len(arr2)) % 2 == 0:
                 # we need to find a number that is next to the biggest one in the left half
                 next_to_median = min(arr1[arr1_right_first_index], arr2[arr2_right_first_index])
